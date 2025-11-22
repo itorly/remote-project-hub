df79d06820475679d328e1af39bed1385c28e16c
itorly <itorly@163.com>
2025-11-22 22:32:13 +0800

    Application functions

---
+++ src/main/java/com/itorly/rph/RemoteProjectHubApplication.java
@@ -0,0 +1,13 @@
+package com.itorly.rph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteProjectHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteProjectHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itorly/rph/RemoteProjectHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itorly.rph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteProjectHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
