c31f5715deadd72ac3a3ed8efd1a2b08e9dd910a
itorly <itorly@163.com>
2025-11-22 23:52:20 +0800

    feat(user): add user entity and repository

---
+++ src/main/java/com/itorly/rph/user/User.java
@@ -0,0 +1,34 @@
+package com.itorly.rph.user;
+
+import com.itorly.rph.common.BaseEntity;
+import com.itorly.rph.organization.OrganizationMember;
+import com.itorly.rph.project.Task;
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false, length = 100)
+    private String displayName;
+
+    @Column(nullable = false, length = 50)
+    private String timezone = "UTC";
+
+    @OneToMany(mappedBy = "user")
+    private Set<OrganizationMember> memberships = new HashSet<>();
+
+    @OneToMany(mappedBy = "assignee")
+    private List<Task> assignedTasks;
+
+    // getters/setters
+}
