4bdf6fbbfe01a47296f0b1526463f175bd992123
itorly <itorly@163.com>
2025-11-25 23:55:54 +0800

    Auth DTO: AuthResponse

---
+++ src/main/java/com/itorly/rph/auto/dto/AuthResponse.java
@@ -0,0 +1,21 @@
+package com.itorly.rph.auto.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AuthResponse {
+
+    private String token;
+    private Long userId;
+    private String email;
+    private String displayName;
+
+    public AuthResponse(String token, Long userId, String email, String displayName) {
+        this.token = token;
+        this.userId = userId;
+        this.email = email;
+        this.displayName = displayName;
+    }
+
+    // getters
+}
