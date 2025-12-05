b9ab343b8614a68b6b4167e8a92b1e8b0db39b6f
itorly <itorly@163.com>
2025-12-05 23:58:05 +0800

    @EqualsAndHashCode and @Data on Organization

--- src/main/java/com/itorly/rph/organization/Organization.java
+++ src/main/java/com/itorly/rph/organization/Organization.java
@@ -4,13 +4,17 @@ import com.itorly.rph.common.BaseEntity;
 import com.itorly.rph.project.Project;
 import com.itorly.rph.user.User;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "organizations")
+@Data
 public class Organization extends BaseEntity {
 
     @Column(nullable = false, length = 255)
@@ -29,5 +33,4 @@ public class Organization extends BaseEntity {
     @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Project> projects;
 
-    // getters/setters
 }
