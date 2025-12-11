62c24a2653025997c7793cfcb449212c9bd67ea5
itorly <itorly@163.com>
2025-12-11 23:59:51 +0800

    Add missing repositories

---
+++ src/main/java/com/itorly/rph/project/BoardColumnRepository.java
@@ -0,0 +1,10 @@
+package com.itorly.rph.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardColumnRepository extends JpaRepository<BoardColumn, Long> {
+
+    List<BoardColumn> findByProjectIdOrderByPositionAsc(Long projectId);
+}
