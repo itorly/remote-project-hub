193be8aba12485e0065730102ebe80aefb4759c2
itorly <itorly@163.com>
2025-12-22 18:10:58 +0800

    Fix GitHub Actions test failure

--- src/test/java/com/itorly/rph/RemoteProjectHubApplicationTests.java
+++ src/test/java/com/itorly/rph/RemoteProjectHubApplicationTests.java
@@ -2,8 +2,10 @@ package com.itorly.rph;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RemoteProjectHubApplicationTests {
 
     @Test
