7dc381c9042e1da6e7efe273e659de60bf3d5e88
itorly <itorly@163.com>
2026-01-01 23:58:37 +0800

    Apache 2.0 license

--- src/main/java/com/itorly/rph/config/SpringDocConfig.java
+++ src/main/java/com/itorly/rph/config/SpringDocConfig.java
@@ -3,6 +3,7 @@ package com.itorly.rph.config;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +15,8 @@ public class SpringDocConfig {
         return new OpenAPI().info(new Info()
                         .title("My API Documents")
                         .description("Spring Boot 3 API Documentation")
-                        .version("v1.0.0"))
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
                 .externalDocs(new ExternalDocumentation()
                         .description("More documents")
                         .url("https://springdoc.org"));
